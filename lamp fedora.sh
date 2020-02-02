0f2b78ac479675ac6cf1040f3b6e8dc80023e387
Ivens Ramon <ivens-oliveira@hotmail.com>
2020-02-02 18:58:15 -0300

    Instalando-lamp-fedora/

---
+++ lamp fedora.sh
@@ -0,0 +1,31 @@
+#Para limpeza do sistema-derivados-debian
+#Autor:Ivens Ramon
+#sistemas Derivados Debian
+#Email:ivens-oliveira@hotmail.com
+#=================================================================================
+
+#!/bin/bash
+
+sudo dnf install httpd -y
+
+sudo systemctl start httpd
+
+sudo systemctl enable httpd
+
+sudo dnf install php php-mysqlnd php-mbstring php-mcrypt -y
+
+sudo systemctl restart httpd
+
+sudo dnf install mariadb-server -y
+
+sudo systemctl start mariadb
+
+sudo systemctl enable mariadb
+
+sudo mysql_secure_installation -y
+
+sudo dnf install phpMyAdmin -y
+
+sudo systemctl restart httpd
+
+exit 0
